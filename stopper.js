eae3c85cf71e21766e3d29ebcb6281d98d1e8d00
Simon MWEPU <simonmwepu@gmail.com>
2019-05-16 12:05:11 +0200

    [ADD]try to fix bug

--- stopper.js
+++ stopper.js
@@ -1,6 +1,16 @@
 function activateStopper(){
     
     PythonShell = require('python-shell');
+    path = require('path');
     
+    var options = {
+        scriptPath: path.join(__dirname, '/'),
+        pythonPath: 'usr/bin/python3'
+    }
+
+    PythonShell.PythonShell.run('main.py', options, function(err, results){
+        if (err) throw err;
+        console.log('results: %j', results);
+    });
 
 }
